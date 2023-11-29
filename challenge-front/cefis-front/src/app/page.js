f8124c0b1405c1e000eaa718d3ae6895a4c5884e
Pedropudin <pedrofapen@gmail.com>
2023-11-28 22:01:10 -0300

    Add body-parser

--- challenge-front/cefis-front/src/app/page.js
+++ challenge-front/cefis-front/src/app/page.js
@@ -1,36 +1,16 @@
-// pages/index.js
-import { useState } from 'react';
-
-function HomePage() {
-  const [userInput, setUserInput] = useState(0);
-
-  const handleInputChange = (event) => {
-    setUserInput(event.target.value);
-  };
-
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-      <p>Welcome to my Next.js app.</p>
-      
-      <div>
-        <label htmlFor="userInput">Enter Text:</label>
-        <input
-          type="text"
-          id="userInput"
-          value={userInput}
-          onChange={handleInputChange}
-        />
-      </div>
-
-      <p>You entered: {userInput}</p>
-
-      {/* No Link component for simplicity */}
-      <a href="/about">
-        <button>Go to About Page</button>
-      </a>
-    </div>
-  );
+const Title = ({text}) => {
+  return <h1 className="bigTitle">{text}</h1>;
 }
 
-export default HomePage;
+const Button = ({text,link}) => {
+  return <button className="square">{text}</button>
+};
+
+export default function page() {
+  return (
+    <>
+    <Title text="Nome"/>
+    <Button text="name"/>
+    </>
+  )
+}
