4bdc3fa4ed539aeeb8c099f7ce50af6e952862ca
Pedropudin <pedrofapen@gmail.com>
2023-12-06 21:28:40 -0300

    Basic login system

--- challenge-front/cefis-front/app/login/page.js
+++ challenge-front/cefis-front/app/login/page.js
@@ -3,30 +3,31 @@
 import TextBox from "../elements/TextBox-user"
 import Link from "next/link";
 
-let cond = 0;
+import { useRouter } from "next/navigation";
 
 export default function Mypage () {
-    async function login(formData) {
-    try {
-        const res = await fetch("http://localhost:8000/user",{
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData),
+
+    const router = useRouter();
+
+    async function login(userInfo) {
+        fetch("http://localhost:8000/user")
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            let userComp;
+            for(let i=0;i<data.all_users.length;i++){
+                userComp = data.all_users[i];
+                if(userInfo.first_name === userComp.first_name && userInfo.last_name === userComp.last_name) {
+                    router.push("http://localhost:3000/users?id=" + userComp.user_id);
+                }
+            };
         });
-        console.log("Server response:",res);
-        cond = 1;
-    } catch (error) {
-        cond = -1;
-        console.error("There was an unexpected error:",error);
     }
-};
-    console.log(cond);
 
     return(
         <div align="center">
-            <h1>Sign-in</h1>
+            <h1>login</h1>
             <TextBox onSubmit={login}/>
             <p>Ainda não tem uma conta? Não perca tempo</p>
             <Link href="http://localhost:3000/signup">
